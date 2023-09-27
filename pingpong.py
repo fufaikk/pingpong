e4f0b09bd024d37869140e826832c4a571e026a3
fufaikk <146011279+fufaikk@users.noreply.github.com>
2023-09-27 19:25:49 +0300

    мяч отскакивает. не работают надписи

--- pingpong.py
+++ pingpong.py
@@ -37,30 +37,45 @@ class Player(GameSprite):
 racket = Player('прямоугольник.png', 100, 20, 4, 40, 100)
 racket2 = Player('прямоугольник.png', 600, 400, 4, 40, 100)
 
-ball = GameSprite('мяч.png', 350, 250, 4, 30, 30)
+ball = GameSprite('мяч.png', 350, 250, 4, 50, 50)
 
 font.init()
 font1 = font.SysFont('Arial', 36)
-text_win = font1.render('YOU WIN', True, (0, 255, 0))
-text_lose = font1.render('YOU LOSE', True, (255, 0, 0))
+text_1 = font1.render('PLAYER 1 LOSE', True, (255, 0, 0))
+text_2 = font1.render('PLAYER 2 LOSE', True, (255, 0, 0))
 
 num_fire = 0
 rel_time = False
+
+speed_x = 3
+speed_y = 3
 
 game = True
 finish = False
 
 while game:
     if not finish:
-        pass
-    window.fill((160,222,153))
-    ball.reset()
-    racket.update_l()
-    racket2.update_r()
-    racket.reset()
-    racket2.reset()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        window.fill((160, 222, 153))
+        ball.reset()
+        racket.update_l()
+        racket2.update_r()
+        racket.reset()
+        racket2.reset()
+    if ball.rect.y > 450 or ball.rect.y < 0:
+        speed_y *= -1
+    if sprite.collide_rect(racket, ball) or sprite.collide_rect(racket2, ball):
+        speed_x *= -1
+        if ball.rect.x < 0:
+            window.blit(text_1, (200, 200))  
+            finish = True
+        if ball.rect.x > 700:
+            window.blit(text_2, (200, 200)) 
+            finish = True  
     for i in event.get():
         if i.type == QUIT:
             game = False
     display.update()
     clock.tick(FPS)
+
